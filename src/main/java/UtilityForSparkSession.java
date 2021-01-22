e35b6c2ba9ab6d7866d0f40332d470acf2f9c9c3
Yassine Boujnah <yassineboujnah@gmail.com>
2021-01-22 17:21:05 +0100

    dwdas

---
+++ src/main/java/UtilityForSparkSession.java
@@ -0,0 +1,14 @@
+import org.apache.spark.sql.SparkSession;
+
+public class UtilityForSparkSession {
+	public static SparkSession mySession() {
+		SparkSession spark = SparkSession		
+	            	.builder()
+	      		.master("local[*]")
+	      		.config("spark.sql.warehouse.dir", "E:/Exp/") // change accordingly
+	      		.appName("Bitcoin Preprocessing")
+	      		.getOrCreate();
+		return spark;
+	}
+
+}
